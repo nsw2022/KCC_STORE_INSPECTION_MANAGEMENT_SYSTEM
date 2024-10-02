eba114cc0090d36a500ecc73398d172761e2493e
Jaewonnnn <yjw970710@gmail.com>
2024-10-02 14:32:01 +0900

    [Test] CICD Test

---
+++ backend/src/main/java/com/sims/admin/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.sims.admin.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+    @GetMapping("/admin")
+    public ResponseEntity<String> admin() {
+
+        return new ResponseEntity<>("Test", HttpStatus.OK);
+    }
+}
