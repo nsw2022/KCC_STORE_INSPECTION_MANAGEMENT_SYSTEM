013f3742a10614ee9b29f92a6794158429adfed2
WonSeungUn <dnjstmddjs12@naver.com>
2024-10-08 11:01:18 +0900

    [Feat]: 로그인 페이지 제작, 가맹점 점검 일정 조회 페이지 제작 #12

--- backend/src/main/java/com/sims/main/controller/MainController.java
+++ backend/src/main/java/com/sims/main/controller/MainController.java
@@ -10,5 +10,10 @@ public class MainController {
     public String index() {
         return "index";
     }
+    
+    @GetMapping("/login")
+    public String login() {
+    	return "login";
+    }
 
 }
