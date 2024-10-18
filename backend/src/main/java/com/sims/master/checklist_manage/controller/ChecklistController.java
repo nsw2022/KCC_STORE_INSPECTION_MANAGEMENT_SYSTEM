b6e88212cd4fe2f46242a66c43d0bccdbdab0a03
dd7672 <dd7672@gmail.com>
2024-10-18 16:07:54 +0900

    [HOTFIX]: 폴더구조 변경

--- backend/src/main/java/com/sims/master/checklist_manage/controller/ChecklistController.java
+++ backend/src/main/java/com/sims/master/checklist_manage/controller/ChecklistController.java
@@ -1,4 +1,4 @@
-package com.sims.master.checklist_manage.controller.controller;
+package com.sims.master.checklist_manage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
